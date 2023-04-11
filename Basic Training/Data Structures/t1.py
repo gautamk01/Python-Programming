b2214f5faae087f75a867fb0afb1547744889143
gautamk01 <charmingdeveloper@protonmail.com>
2023-04-11 21:33:51 +0530

    Just added 2 classes

---
+++ Basic Training/Data Structures/t1.py
@@ -0,0 +1,13 @@
+letters = ["a", "b", "c"]
+martix = [[1, 2], [3, 4]]
+zeros = [0] * 5
+print(zeros)    # [0, 0, 0, 0, 0]
+combined = zeros + letters
+print(combined)  # [0, 0, 0, 0, 0, 'a', 'b', 'c']
+numbers = list(range(20))
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+print(numbers)
+chars = list("Hello World")
+print(chars)  # ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd']
+# length
+print(len(chars))  # 11
